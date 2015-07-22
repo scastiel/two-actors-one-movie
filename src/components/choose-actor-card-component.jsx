5188a22faaea5369445812eeda6ad4972caf210f
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-07-22 18:43:08 +0200

    Correction on text input margin

--- src/components/choose-actor-card-component.jsx
+++ src/components/choose-actor-card-component.jsx
@@ -9,11 +9,15 @@ export default class ChooseActorCardComponent extends DefaultCardComponent {
 		super(props);
 		this.styles.textField = {
 			width: '100%',
-			marginTop: '-20px'
+			marginTop: '-20px',
+			marginBottom: '-20px'
 		};
 		this.styles.textFieldLabel = {
 			color: 'rgba(255, 255, 255, 0.4)'
 		};
+	}
+	handleFocus(event) {
+		
 	}
 	render() {
 		return (
@@ -22,7 +26,7 @@ export default class ChooseActorCardComponent extends DefaultCardComponent {
 				<div className="mdl-card__actions" style={this.styles.title}>
 					<form>
 						<div className="mdl-textfield mdl-js-textfield" style={this.styles.textField}>
-							<input className="mdl-textfield__input" type="text" id="sample1" />
+							<input className="mdl-textfield__input" type="text" id="sample1" onFocus={this.handleFocus}/>
 							<label className="mdl-textfield__label" htmlFor="sample1"
 							       style={this.styles.textFieldLabel}>
 								Type some actor name...
